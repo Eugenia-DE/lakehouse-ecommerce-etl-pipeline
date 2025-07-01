802022179a01654dbeabd4f0573ed3e4e7ce59c3
Eugenia-DE <eugenia.teye@amalitechtraining.org>
2025-07-01 15:49:43 +0000

    Remove trailing whitespaces.

--- glue_jobs/archive_and_mark_processed.py
+++ glue_jobs/archive_and_mark_processed.py
@@ -6,10 +6,11 @@ from pyspark.context import SparkContext
 from awsglue.context import GlueContext
 from awsglue.job import Job
 
+
 def main():
     args = getResolvedOptions(sys.argv, ["JOB_NAME", "raw_keys", "dataset_names"])
     JOB_NAME = args["JOB_NAME"]
-    RAW_KEYS = args["raw_keys"].split(",")         
+    RAW_KEYS = args["raw_keys"].split(",")
     DATASET_NAMES = args["dataset_names"].split(",")
 
     BUCKET = "ecommerce-lakehouse-project"
@@ -25,16 +26,19 @@ def main():
         print(f"Archiving and marking: {raw_key} | dataset: {dataset}")
         copy_source = {"Bucket": BUCKET, "Key": raw_key}
         archive_key = f"archived/{raw_key}"
-        log_key = f"processed/_processed_log/{dataset}/{os.path.basename(raw_key)}.txt"
+        log_key = (
+            f"processed/_processed_log/{dataset}/{os.path.basename(raw_key)}.txt"
+        )
 
         try:
             bucket.copy(copy_source, archive_key)
             s3.Object(BUCKET, raw_key).delete()
             s3_client.put_object(Bucket=BUCKET, Key=log_key, Body="processed")
-            print(f" Archived {raw_key} and created marker {log_key}")
+            print(f"Archived {raw_key} and created marker {log_key}")
         except Exception as e:
-            print(f" Failed to archive/mark {raw_key}:", str(e))
+            print(f"Failed to archive/mark {raw_key}:", str(e))
 
     job.commit()
+
 
 main()
